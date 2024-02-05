ad5e66ced26b8358b103008f9cdca1d34b92d5b6
My name <09aryan.lucky@gmail.com>
2024-02-05 20:03:36 +0530

    JavaScript

---
+++ sun-script.js
@@ -0,0 +1,144 @@
+//Conditions or Descions
+
+//There are three different ways of writhing conditions:-
+//if else
+//switch case
+//Ternary operator or Ternary conditions
+
+
+//Rating mini app
+
+// let rating=0;
+
+// if(rating==5){
+//     console.log('Excellent,you get 5 stars')
+// }
+// else if(rating==4){
+//     console.log('Very Good, you get 4 stars')
+// }
+// else if(rating==3){
+//     console.log('Good, you got 3 stars')
+// }
+// else{
+//     console.log('You need to improve your work.')
+// }
+
+//Switch case 
+//(work is same, but the way of writing is differnt as compare to if else)
+
+// let user='admin'
+
+// switch(user){
+//     case 'admin':
+//         console.log('Redirect him/her to the admin page');
+//         break;
+//     case 'mentor':
+//         console.log('Redirect him/her to the mentor page');
+//         break;
+//     default:
+//         console.log('I am a simple user')    
+
+// }
+
+//Ternary operations or conditions
+
+//Condition ?True statement :False statement
+
+// let raining= true;
+
+// raining ? console.log('It is Raining') : console.log('It is not Rainig')
+// let raining= true;
+
+// raining ? console.log('It is Raining') : console.log('It is not Rainig')
+
+// let userlogin= false;
+
+// userlogin ? console.log('log out') : console.log('log in')
+
+
+//Array
+
+// let name = ["Aryan", "Aditya", "Tarun", "Arun"]
+// console.log(name)
+// console.log(name[0])
+
+
+//Object
+
+// let bankaccount= {firstname:"Aryan",
+//                   lastname:"Saini",
+//                   mobileno:"978455XXXX",
+//                   balance:"10000",
+// };
+// console.log(bankaccount)
+// console.log(bankaccount.balance)
+
+
+//Date and Math
+
+// const now= new Date();
+// console.log(now)
+// console.log(now.toString());
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDay());
+// console.log(now.getTime());
+// console.log(now.getHours())
+// console.log(now.getMinutes())
+// console.log(now.getSeconds())
+
+
+// MATH
+
+// const pi=Math.PI
+// console.log(pi)
+// console.log(Math.round(pi));
+// console.log(Math.floor(9.4));
+// console.log(Math.ceil(9.4));
+
+//Min and Max
+// console.log(Math.min(1,2,3,4,5));
+// console.log(Math.max(1,2,3,4,5));
+
+//Random
+// console.log(Math.random())  //0 - 0.9999999999 (0 -1)
+// console.log(Math.random()*10)
+// console.log(Math.round(Math.random()*50));
+
+//
+// console.log(Math.pow(3,3));
+
+// console.log(Math.log(2));
+// console.log(Math.sin(0));
+
+
+// LOOPS
+// do while, while, for
+
+// i=6;
+// do {
+//     console.log('Value of i is :', i)
+//     i++;
+// }while(i<=5);
+
+// j=6;
+// while(j<=5){
+//     console.log('Value of j is :', j);
+//     j++;
+// }
+
+// for(i=0; i<=5; i++){
+//     console.log('Value of i is:',i);
+// }
+
+// for(i=0; i<=10; i++){
+//     console.log(`${i} * ${i} = ${i*i}`);
+// }
+
+
+
+
+
+
+
+
