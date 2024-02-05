f82fed6431fab5e8a03e8b68b7809719bacf834c
My name <09aryan.lucky@gmail.com>
2024-02-05 19:56:25 +0530

    js

---
+++ sat-script.js
@@ -0,0 +1,120 @@
+// 7;
+// 2.5;
+// //Numer values {both are treated as numbers}
+
+// "Aryan";
+// 'Aryan';
+// //String values {double quotes or single quotes}
+
+// true;
+// false;
+// //Boolean values
+
+// null;
+// undefined;
+// //Empty values
+
+// //All are called as Primitive values.
+
+
+// //Array and Objects
+
+// //Array
+// [1,2,3,4,5,6,7] //Includes sigle value or data type.
+// [1,"aryan","aditya","tarun","arun"] //Includes different values or data types.
+
+// //Objects (includes a key value pair)
+// {
+//     fname:Aryan;
+//     lname:Saini;
+// }
+
+// //Both are considered as non-primitive data types.
+
+
+
+//Variable:- Place holder for the values
+
+// There are three ways to declare variable.
+// var, let, 
+// const(constant means, value of something does not change over a period of time.)
+
+// var x=25;
+// let y=51;
+// const z=Aryan;
+
+//For good practice, use "var" or "const".
+
+// let firstname = "Aryan"
+// let lastname = "Saini"
+// const mobileno = 9784550000
+
+// console.log("My first name is",firstname);
+// console.log("My last name is",lastname);
+// console.log("My mobile no. is",mobileno);
+
+//Template literal
+// console.log(`My current first name is ${firstname} ${lastname} ${mobileno}`)
+
+// OPERATORS 
+
+// Assignment operator
+// let x=11;
+// let y=10;
+// let z=x+y;
+
+// console.log(x+y)
+// console.log(z) 
+//both are correct
+
+// Arithmetic operator
+// +, -, *, /, % =>remainder
+
+// console.log(x+y)
+// console.log(x-y)
+// console.log(x*y)
+// console.log(x/y)
+// console.log(x%y)
+
+
+// Comparison operators
+
+// let abc=10;
+// let xyz=20;
+
+// console.log(abc>xyz)
+// console.log(abc<xyz)
+
+// (==, =>double equal to) and (===, triple equal to)
+// let x=10;
+// let y='10';
+
+// console.log(x==y) //double equal to compares only values not data types.
+// console.log(x===y) //triple equal to compares values as well as data types.
+
+// let y;
+// y=10;
+// console.log(y)
+
+
+// DECISIONS or CONDITIONS
+
+// let age = 18;
+// if (age >= 19) {
+//     console.log('Yes, you can enter in the park.');
+// }
+// else{
+//     console.log('NO')
+// }
+
+// let Standard=6 ;
+// if(Standard==1){
+//     console.log("room one")
+// } else if(Standard==2){
+//     console.log("room two")
+// } else if(Standard==3){
+//     console.log('room three')
+// } else{
+//     console.log("You are the principal")
+// }
+
